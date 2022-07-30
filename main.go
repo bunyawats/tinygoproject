be288ed6b635c72c292998eec6aca397cc7693ae
Bunyawat Singchai <bunyawat.s@gmail.com>
2022-07-30 13:37:20 +0700

    use goroutine

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 const (
 	led  = machine.LED
 	led3 = machine.GP3
-	pin  = machine.GP20
+	led8 = machine.GP8
+	bnt  = machine.GP20
 )
 
 var (
@@ -25,16 +26,30 @@ func configuration() {
 		Mode: machine.PinOutput,
 	})
 
-	pin.Configure(machine.PinConfig{
+	led8.Configure(machine.PinConfig{
+		Mode: machine.PinOutput,
+	})
+
+	bnt.Configure(machine.PinConfig{
 		Mode: machine.PinInputPulldown,
 	})
 
-	pin.SetInterrupt(machine.PinFalling, isr)
+	bnt.SetInterrupt(machine.PinFalling, isr)
 }
 
 func isr(p machine.Pin) {
+
 	state = !state
 	led3.Set(state)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			led8.High()
+			time.Sleep(time.Millisecond * 500)
+			led8.Low()
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
 }
 
 func main() {
