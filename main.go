e98bf8582979b44d81bddc26ca429dbf7922766f
Bunyawat Singchai <bunyawat.s@gmail.com>
2022-07-31 10:24:41 +0700

    isr mush not wait we can wait in goroutine

--- main.go
+++ main.go
@@ -9,11 +9,13 @@ const (
 	ledOnB = machine.LED
 	led3   = machine.GP3
 	led8   = machine.GP8
+	led9   = machine.GP9
 	bnt    = machine.GP20
 )
 
 var (
 	state = true
+	done  = make(chan bool, 1)
 )
 
 func configuration() {
@@ -30,6 +32,10 @@ func configuration() {
 		Mode: machine.PinOutput,
 	})
 
+	led9.Configure(machine.PinConfig{
+		Mode: machine.PinOutput,
+	})
+
 	bnt.Configure(machine.PinConfig{
 		Mode: machine.PinInputPulldown,
 	})
@@ -37,28 +43,41 @@ func configuration() {
 	bnt.SetInterrupt(machine.PinFalling, isr)
 }
 
-func isr(p machine.Pin) {
-
-	state = !state
-	led3.Set(state)
-
-	go blink(led8)
+func waitBlink(led machine.Pin, d chan bool) {
+	<-d
+	blink(led)
+	d <- true
 }
 
 func blink(led machine.Pin) {
-	time.Sleep(time.Second * 3)
+
+	time.Sleep(time.Second * 2)
+
 	for i := 0; i < 10; i++ {
 		led.High()
 		time.Sleep(time.Millisecond * 500)
 		led.Low()
 		time.Sleep(time.Millisecond * 500)
 	}
+
+}
+
+func isr(p machine.Pin) {
+
+	state = !state
+	led3.Set(state)
+
+	go waitBlink(led8, done)
+	go waitBlink(led9, done)
+
 }
 
 func main() {
 
 	configuration()
 	led3.Set(state)
+	done <- true
+
 	for {
 		if state {
 			blink(ledOnB)
